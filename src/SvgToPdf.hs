52e0819067c166fce5d3cd914e07f54633068719
Vitalii Guzeev <viviag@yandex.ru>
2022-06-25 16:23:24 +0300

    Make input of svgToPdf polymorphic
    Reason -- compatibility with servant-multipart types, but this may be useful generally.

--- src/SvgToPdf.hs
+++ src/SvgToPdf.hs
@@ -1,19 +1,31 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 module SvgToPdf where
 
 import Graphics.Rendering.Cairo.LibRSvg
+import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
 import Graphics.Rendering.Cairo (withPDFSurface, renderWith)
 
-loadSvg :: FilePath -> IO Svg
-loadSvg path = do
-  result <- fromBuffer =<< BS.readFile path
+class CanContainInput a where
+  readInput :: a -> IO ByteString
+
+instance CanContainInput FilePath where
+  readInput = BS.readFile
+
+instance CanContainInput ByteString where
+  readInput = return
+
+loadSvg :: CanContainInput a => a -> IO Svg
+loadSvg inp = do
+  result <- fromBuffer =<< readInput inp
   case result of
     Left bs -> fail $ BSC.unpack bs
     Right sp -> pure sp
 
-svgToPdf :: FilePath -> FilePath -> IO ()
+svgToPdf :: CanContainInput a => a -> FilePath -> IO ()
 svgToPdf inp out = do
   svgPtr <- loadSvg inp
   (sizeX, sizeY) <- dimensions svgPtr
